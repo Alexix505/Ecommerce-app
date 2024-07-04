1ee81f69c1ba2ac43c812d5867f1cafe9d14b7ac
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-07-04 20:27:16 +0100

    categories

---
+++ lib/const/global_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// theme color
+Color lightScaffoldColor = const Color(0xfff8f8f8);
+Color lightCardColor = const Color(0xffffffff);
+Color lightBackgroundColor = const Color(0xffd0e8f2);
+Color lightIconsColor = const Color.fromRGBO(255, 175, 0, 120);
+Color lightTextColor = const Color(0xff324558);
